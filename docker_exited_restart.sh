73454f300bfc872daf3a18140be639f5fea38b0a
antstars <antstars@outlook.com>
2024-04-21 16:33:00 +0800

    add:检测并重启docker停止状态的容器

---
+++ docker_exited_restart.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+###更新docker后有些服务会停止，需要执行重启，此脚本实现自动化重启###
+###写入定时任务每两分钟执行一次：*/2 * * * * {path}/docker_exited_restart.sh
+
+# 检查是否以管理员权限运行脚本
+if [ "$(id -u)" != "0" ]; then
+    echo "必须以 root 或 sudo 身份运行此脚本。"
+    exit 1
+fi
+
+exited_container=$(docker ps -a | grep Exited | awk '{print $1}')
+
+for container_id in ${exited_container}; do
+    if [ -n "${exited_container}" ]; then
+        docker restart ${container_id}
+        echo "$(date '+%Y-%m-%d %H:%M:%S') 容器：${container_id} 已重启" >> /var/log/docker_exited_restart.log
+    fi
+done
+
